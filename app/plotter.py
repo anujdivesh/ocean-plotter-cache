d99fedbb8b5d4214e25a29b50ce9f8f8ad507b02
Anuj Divesh <divesha@spc.int>
2025-09-15 12:31:06 +1200

    added salinity fix

--- app/plotter.py
+++ app/plotter.py
@@ -1953,7 +1953,7 @@ class Plotter:
         file_name = f"{prefix}{infix_formatted}{suffix}"
         if not file_name.endswith('.nc'):
             file_name += '.nc'
-        if layer_id == "16" or layer_id == "6" or layer_id == "27" or layer_id == "29":
+        if layer_id == "16" or layer_id == "6" or layer_id == "27" or layer_id == "29" or layer_id == "3":
             file_name = 'latest.nc'
         if layer_id == "2" or layer_id =="10" or layer_id =="11" or layer_id =="12" or layer_id =="14":
             file_name = 'latest_merged.nc'
