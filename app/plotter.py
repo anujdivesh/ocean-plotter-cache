2a065b8e95b2dffc5d698c0c95f342180c6a8a87
Anuj Divesh <divesha@spc.int>
2025-09-19 10:28:23 +1200

    added phytoplankton

--- app/plotter.py
+++ app/plotter.py
@@ -1965,7 +1965,7 @@ class Plotter:
         file_name = f"{prefix}{infix_formatted}{suffix}"
         if not file_name.endswith('.nc'):
             file_name += '.nc'
-        if layer_id == "16" or layer_id == "6" or layer_id == "27" or layer_id == "29" or layer_id == "3" or layer_id == "53":
+        if layer_id == "16" or layer_id == "6" or layer_id == "27" or layer_id == "29" or layer_id == "3" or layer_id == "53" or layer_id == "28":
             file_name = 'latest.nc'
         if layer_id == "2" or layer_id =="10" or layer_id =="11" or layer_id =="12" or layer_id =="14":
             file_name = 'latest_merged.nc'
